cf6023e73f5c2525f15c0079bc1e63088ff6569f
ELrasmElknk <110526070+ELrasmElknk@users.noreply.github.com>
2022-08-18 05:49:09 +0200

    Add files via upload

--- JE313P/plugins/locks.py
+++ JE313P/plugins/locks.py
@@ -13,7 +13,7 @@ LOCKS_HELP = """
 
 الصلاحيات
 لعرض الصلاحيات التي يمكنك قفلها
-[𖠄 𝗝𝗲𝗽𝘁𝗵𝗼𝗻 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𖠄](https://t.me/JEPTHON)
+[𖠄 ELRASAM 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𖠄](https://t.me/EL_RASA)
 """
 
 @JE313P.on(events.NewMessage(pattern="^[!?/]قفل ?(.*)"))
--- JE313P/plugins/ping.py
+++ JE313P/plugins/ping.py
@@ -5,12 +5,12 @@ from telethon import Button, events
 from JE313P import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/2ad68bd0e391a69163d0a.jpg"
+    "PING_PIC", "https://telegra.ph/file/21e89e713e2e7de5cc936.jpg"
 )
 ms = 4
 
 ALIVE = os.environ.get(
-    "ALIVE", "@lMl10l"
+    "ALIVE", "@Elrasam7"
 )
 
 CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
@@ -18,5 +18,5 @@ CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
 
 @JE313P.on(events.NewMessage(pattern="^/بنك"))
 async def _(event):
-    UMM = [[Button.url("السورس", "https://t.me/JEPTHON")]]
+    UMM = [[Button.url("السورس", "https://t.me/EL_RASA")]]
     await JE313P.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
